eaf6c0c2c1944221845f1c50294a571b5d53805b
unknown <Ashish.Khadase@ATMECSINDT-068.ATMECS.CORP>
2015-03-26 15:56:39 +0530

    initial commit

---
+++ src/Trail.java
@@ -0,0 +1,28 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Trail {
+
+	public static void main(String[] args) {
+
+		
+		String idate = "07/04/2015";
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Date inputDate = null;
+		try {
+			inputDate = sdf.parse(idate);
+			System.out.println(inputDate.toString());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SimpleDateFormat sdfo = new SimpleDateFormat("MMM dd");
+		String outputDate = sdfo.format(inputDate);
+		System.out.println("ouptu"+outputDate);
+		
+	}
+
+}
